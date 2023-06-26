f4d5dbd5714f2e72139a187a3af0d1668628a214
AlexandrKoba <babakoba@yandex.ru>
2023-06-26 22:26:07 +0700

    first commit

--- Ganga.py
+++ Ganga.py
@@ -1,72 +1,99 @@
-import threading
+import pygame
 import time
 
+# Инициализация Pygame
+pygame.init()
 
-class MarijuanaBush:
-    def __init__(self):
-        self.health = 100
-        self.thirst = 0
-        self.fertilizer = 0
-        self.temperature = 25
+# Определение размеров окна
+window_width = 400
+window_height = 300
 
-    def water(self):
-        self.thirst -= 10
-        if self.thirst < 0:
-            self.thirst = 0
-        self.health += 20
-        if self.health > 100:
-            self.health = 100
+# Создание окна
+window = pygame.display.set_mode((window_width, window_height))
+pygame.display.set_caption("Выращивание куста марихуаны")
 
-    def fertilize(self):
-        self.fertilizer += 1
-        self.health += 10
-        if self.health > 100:
-            self.health = 100
+# Определение цветов
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
 
-    def adjust_temperature(self, temperature):
-        self.temperature = temperature
+# Определение параметров куста марихуаны
+bush_health = 100
+bush_thirst = 0
+bush_fertilizer = 0
+bush_temperature = 25
 
-    def simulate_growth(self):
-        while self.health > 0:
-            self.health -= 1
-            self.thirst += 1
-            if self.thirst > 100:
-                self.health -= 10
-            if self.fertilizer > 0:
-                self.health += 2
-                self.fertilizer -= 1
-            if self.temperature < 20 or self.temperature > 30:
-                self.health -= 5
+# Определение размеров и позиции прямоугольников для отображения состояния куста
+health_rect = pygame.Rect(50, 50, 200, 20)
+thirst_rect = pygame.Rect(50, 100, 200, 20)
+fertilizer_rect = pygame.Rect(50, 150, 200, 20)
+temperature_rect = pygame.Rect(50, 200, 200, 20)
 
-            print(
-                f"Жизнь: {self.health}, Жажда: {self.thirst}, Удобрения: {self.fertilizer}, Температура: {self.temperature}")
-            time.sleep(1)
+# Основной игровой цикл
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
 
-        print("Ваш куст марихуаны погиб...")
+    # Обработка взаимодействия с кустом марихуаны
+    if pygame.mouse.get_pressed()[0]:  # Левая кнопка мыши нажата
+        mouse_x, mouse_y = pygame.mouse.get_pos()
 
+        # Проверка, была ли нажата кнопка внутри прямоугольника состояния жизни
+        if health_rect.collidepoint(mouse_x, mouse_y):
+            bush_health += 20
+            if bush_health > 100:
+                bush_health = 100
 
-# Создаем экземпляр куста марихуаны
-bush = MarijuanaBush()
+        # Проверка, была ли нажата кнопка внутри прямоугольника состояния жажды
+        if thirst_rect.collidepoint(mouse_x, mouse_y):
+            bush_thirst += 10
+            if bush_thirst > 100:
+                bush_thirst = 100
 
-# Запускаем симуляцию роста куста марихуаны в отдельном потоке
-growth_thread = threading.Thread(target=bush.simulate_growth)
-growth_thread.start()
+        # Проверка, была ли нажата кнопка внутри прямоугольника состояния удобрений
+        if fertilizer_rect.collidepoint(mouse_x, mouse_y):
+            bush_fertilizer += 10
+            if bush_fertilizer > 100:
+                bush_fertilizer = 100
 
-# Взаимодействие с пользователем
-while True:
-    print("Выберите действие:")
-    print("1. Полить куст марихуаны")
-    print("2. Удобрить куст марихуаны")
-    print("3. Изменить температуру")
-    print("0. Выйти")
+        # Проверка, была ли нажата кнопка внутри прямоугольника состояния температуры
+        if temperature_rect.collidepoint(mouse_x, mouse_y):
+            bush_temperature += 5
+            if bush_temperature > 40:
+                bush_temperature = 40
 
-    выбор = input("Введите номер действия: ")
+    # Отображение состояния куста марихуаны
+    window.fill(BLACK)
 
-    if выбор == "1":
-        bush.water()
-        print("Куст марихуаны полит и поблагодарил вас!")
-    elif выбор == "2":
-        bush.fertilize()
-        print("Куст марихуаны удобрен и вырос в размере!")
+    # Отображение прямоугольников состояния куста
+    pygame.draw.rect(window, WHITE, health_rect)
+    pygame.draw.rect(window, WHITE, thirst_rect)
+    pygame.draw.rect(window, WHITE, fertilizer_rect)
+    pygame.draw.rect(window, WHITE, temperature_rect)
 
-2
+    # Отображение текущих значений состояния куста
+    pygame.draw.rect(window, GREEN, (health_rect.x, health_rect.y, bush_health * 2, health_rect.height))
+    pygame.draw.rect(window, RED, (thirst_rect.x, thirst_rect.y, bush_thirst * 2, thirst_rect.height))
+    pygame.draw.rect(window, RED, (fertilizer_rect.x, fertilizer_rect.y, bush_fertilizer * 2, fertilizer_rect.height))
+    pygame.draw.rect(window, WHITE, (temperature_rect.x, temperature_rect.y, (bush_temperature - 20) * 10, temperature_rect.height))
+
+    # Отображение текста в полосках состояния
+    font = pygame.font.Font(None, 20)
+    health_text = font.render(str(bush_health), True, BLACK)
+    thirst_text = font.render(str(bush_thirst), True, BLACK)
+    fertilizer_text = font.render(str(bush_fertilizer), True, BLACK)
+    temperature_text = font.render(str(bush_temperature), True, BLACK)
+
+    window.blit(health_text, (health_rect.x + health_rect.width + 10, health_rect.y))
+    window.blit(thirst_text, (thirst_rect.x + thirst_rect.width + 10, thirst_rect.y))
+    window.blit(fertilizer_text, (fertilizer_rect.x + fertilizer_rect.width + 10, fertilizer_rect.y))
+    window.blit(temperature_text, (temperature_rect.x + temperature_rect.width + 10, temperature_rect.y))
+
+    # Обновление окна
+    pygame.display.flip()
+
+    # Задержка, чтобы не перегружать процессор
+    time.sleep(0.1)
